7e1f16be9472ed57e759718a5b00e4562bd1c72a
Hadjsayhi <Hadjsayhi@gmail.com>
2025-11-08 15:27:57 +0200

    ola kala

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-const file = location.pathname.split('/').pop() || 'home.html';
-document.querySelectorAll('.bottom-nav a').forEach(a=>{
-  const isHome = a.hasAttribute('data-home') && file === '';
-  if (a.getAttribute('href') === file || isHome) a.classList.add('active');
-});
+// ενεργό link στο navbar
+(function () {
+  const file = location.pathname.split('/').pop() || 'home.html';
+  document.querySelectorAll('.bottom-nav a').forEach(a => {
+    const isHome = a.hasAttribute('data-home') && (file === '' || file === 'index.html' || file === 'home.html');
+    if (a.getAttribute('href') === file || isHome) a.classList.add('active');
+  });
+})();
